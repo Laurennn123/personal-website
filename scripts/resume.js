438b20f2044d76f1876c00f546fc10c69d46eea7
Laurennn123 <2021-100715@rtu.edu.ph>
2023-07-24 23:46:36 +0800

    appearing of the navigation bar when it clicks the fa-bar icon

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,3 +1,9 @@
-document.querySelector("#fa-bars-icon").addEventListener("click", function() {
-    document.querySelector("#navigation-bar-container-in-resume").style.width = "100%";
+$("#fa-bar-icon").click(function() {
+    $("#navigation-bar-container-in-resume").css("width", "15%");
+    $("#fa-bar-icon").css("display", "none");
+});
+
+$("#close-button").click(function() {
+    $("#navigation-bar-container-in-resume").css("width", "0%");
+    $("#fa-bar-icon").css("display", "inline-block");
 });
