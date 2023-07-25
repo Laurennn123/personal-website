62f70390dafabc5be175e35046b88516902bd28b
Laurennn123 <2021-100715@rtu.edu.ph>
2023-07-25 18:22:07 +0800

    added function for opening the fa-bar icon

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,9 +1,17 @@
-$("#fa-bar-icon").click(function() {
-    $("#navigation-bar-container-in-resume").css("width", "15%");
-    $("#fa-bar-icon").css("display", "none");
-});
+function changingWidth(width) {
+    $("#fa-bar-icon").click(function() {
+        $("#navigation-bar-container-in-resume").css("width", "" + width + "%");
+        $("#fa-bar-icon").css("display", "none");
+    });
+}
 
 $("#close-button").click(function() {
     $("#navigation-bar-container-in-resume").css("width", "0%");
     $("#fa-bar-icon").css("display", "inline-block");
 });
+
+if(window.matchMedia("(min-width: 576px)").matches) {
+    changingWidth(15);
+} else {
+    changingWidth(30);
+}
