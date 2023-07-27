37941848f9f9161ec973ada9c96af5757dc0f7b9
Laurennn123 <2021-100715@rtu.edu.ph>
2023-07-27 19:10:41 +0800

    initial commit

---
+++ scripts/projects.js
@@ -0,0 +1,5 @@
+function fullScreen() {
+    document.querySelector("#automated-library-system-simulation").requestFullscreen();
+}
+
+document.querySelector("#automated-library-system-simulation").addEventListener("play", fullScreen);
